0327d5d7de2f515473363f111bca31c1cf65ae64
Op Code <suarezdarlin41@gmail.com>
2024-02-18 19:48:40 -0600

    include html function

---
+++ js/include_layout.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", e => {
+    const includeHtml = async (el, url) => {
+        try {
+            let res = await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "text/html; charset= utf-8"
+                }
+            }),
+                html = await res.text();
+
+            if (!res.ok) throw { status: res.status, statusText: res.statusText };
+
+            el.outerHTML = html;
+
+        } catch (err) {
+            let message = err.statusText ||
+                "Error al cargar el archivo, verifica que estas haciendo la petición por http o https";
+            el.outerHTML = `<div><p>Error ${err.status}: ${message}</p></div>`;
+        }
+    }
+
+    document.querySelectorAll("[data-include]").forEach(el => includeHtml(el, el.getAttribute("data-include")));
+});
